01357b10bf71366d4afb09fce2252d2b522bbda1
Liber, Alon <alonliber.in@gmail.com>
2019-12-18 00:29:15 +0200

    remove run.py
    Signed-off-by: Liber, Alon <alonliber.in@gmail.com>

--- app.py
+++ app.py
@@ -3,6 +3,12 @@
 from response import APIResponse
 from flask import Flask, request
 from firebase import Firebase
+from firebase_admin import credentials
+
+import os
+import argparse
+import firebase_admin
+import sys
 
 # initialize flask
 app = Flask("kimhr-api")
@@ -116,3 +122,47 @@ def answers(id_=None):
         data = Firebase(child).get(id_)
 
     return APIResponse(data).make_response()
+
+def firebase_init():
+    """
+    initialize firebase
+    """
+    firebase_admin.initialize_app(credentials.Certificate("firebase-key.json"), {
+        "databaseURL": "https://kimhr-e96a3.firebaseio.com"
+    })
+
+def get_parser():
+    """
+    configure and get parser
+    """
+    parser = argparse.ArgumentParser("kimhr-api")
+
+    parser.add_argument("-d", "--debug", help="run in debug mode", default=False, action="store_true", required=False)
+    parser.add_argument("-p", "--port", help="port number", default=5000, required=False)
+
+    return parser
+
+if __name__ == "__main__":
+    """
+    main function
+    """
+    try:
+        args = get_parser().parse_args()
+
+        if args.debug == True:
+            os.environ["FLASK_ENV"] = "development"
+
+        else:
+            os.environ["FLASK_ENV"] = "production"
+
+        # initialize firebase
+        firebase_init()
+
+        app.run(**{
+            "host": "0.0.0.0",
+            "debug": args.debug,
+            "port": args.port
+        })
+
+    except Exception as e:
+        print(str(e))
